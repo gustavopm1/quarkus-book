145c764688ced37fff347be6fd4e64bd66d05b41
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2025-07-01 18:30:49 -0300

    Working with different profiles

--- src/main/java/org/gustavo/quarkus/starting/BookRepository.java
+++ src/main/java/org/gustavo/quarkus/starting/BookRepository.java
@@ -2,6 +2,7 @@ package org.gustavo.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,12 +10,15 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre", defaultValue = "Novel")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Crime and Punishment", "Fyodor Dostoevsky", 1886, "Literary Fiction"),
-                new Book(1, "David Copperfield", "Charles Dickens", 1850, "Bildungsroman"),
-                new Book(1, "The Hunchback of Notre-Dame", "Victor Hugo", 1831, "Gothic"),
-                new Book(1, "The Posthumous Memoirs of Brás Cubas", "Machado de Assis", 1881, "Novel")
+                new Book(1, "Crime and Punishment", "Fyodor Dostoevsky", 1886, genre),
+                new Book(1, "David Copperfield", "Charles Dickens", 1850, genre),
+                new Book(1, "The Hunchback of Notre-Dame", "Victor Hugo", 1831, genre),
+                new Book(1, "The Posthumous Memoirs of Brás Cubas", "Machado de Assis", 1881, genre)
         );
     }
 
